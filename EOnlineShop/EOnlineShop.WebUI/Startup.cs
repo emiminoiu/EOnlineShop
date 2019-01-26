53358bd13296db9e371177c09d933d7e2a34238c
Emi Minoiu <minoiu_emi@yahoo.com>
2019-01-26 08:07:08 +0000

    Initial Commit

---
+++ EOnlineShop/EOnlineShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EOnlineShop.WebUI.Startup))]
+namespace EOnlineShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

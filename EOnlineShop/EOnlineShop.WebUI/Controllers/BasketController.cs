83fdd06fa952c5ea9668f1eba586fce9a198fdbf
Emi Minoiu <minoiu_emi@yahoo.com>
2019-03-04 08:03:47 +0000

    Updating

--- EOnlineShop/EOnlineShop.WebUI/Controllers/BasketController.cs
+++ EOnlineShop/EOnlineShop.WebUI/Controllers/BasketController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EOnlineShop.core.Models;
+using EOnlineShop.Core.Contracts;
 using EOnlineShop.Services;
 
 namespace EOnlineShop.WebUI.Controllers
@@ -13,11 +14,12 @@ namespace EOnlineShop.WebUI.Controllers
     {
         IBasketService basketService;
         IOrderInterface orderService;
-
-        public BasketController(IBasketService BasketService, IOrderInterface orderService)
+        private IRepository<Customer> customers;
+        public BasketController(IBasketService BasketService, IOrderInterface orderService, IRepository<Customer> customers)
         {
             this.basketService = BasketService;
             this.orderService = orderService;
+            this.customers = customers;
         }
         // GET: Basket2
         public ActionResult Index()
@@ -46,20 +48,42 @@ namespace EOnlineShop.WebUI.Controllers
 
             return PartialView(basketSummary);
         }
-
+        [Authorize]
         public ActionResult Checkout1()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    City = customer.City,
+                    State = customer.State,
+                    ZipCode = customer.ZipCode,
+                    Street = customer.Street
+
+
+                };
+                return View(order);
+            }
+            else
+            {
+                return View();
+            }
         }
+        [Authorize]
         [HttpPost]
         public ActionResult Checkout1(Order order)
         {
             var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
             order.OrderStatus = "Order Processed";
-            orderService.CreateOrder(order,basketItems);
+            order.Email = User.Identity.Name;
+            orderService.CreateOrder(order, basketItems);
             //basketService.ClearBasket(this.HttpContext);
-            return RedirectToAction("ThankYou", new{ OrderId = order.Id } );
+            return RedirectToAction("ThankYou", new { OrderId = order.Id });
         }
 
         public ActionResult ThankYou(string OrderId)
